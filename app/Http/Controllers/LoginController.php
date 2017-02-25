c49aefe6cf0fff06a642c156b5af7208a208aefc
Jhon <yc24.95@gmail.com>
2017-02-25 02:52:38 -0500

    primera version del envio de json parece que esta bien ojala que asi sea

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,10 +8,51 @@ class LoginController extends Controller
 {
 
   public function ObtenerDatos(Request $request){
+
     $user = $request->input('user');
     $password = $request->input('password');
-    echo "El nombre de usuario es: ".$user."<br>";
-    echo "La clave de usuario es: ".$password;
+    //echo "El nombre de usuario es: ".$user."<br>";
+    //echo "La clave de usuario es: ".$password;
+
+    //API Url
+    $url = 'https://echo.getpostman.com/post';
+
+    //Initiate cURL.
+    $ch = curl_init($url);
+
+    //The JSON data.
+    $jsonData = array(
+        'Vendor' => 'ruckus',
+        'RequestPassword' => 'myPassword',
+        'APIVersion' => '1.0',
+        'RequestCategory' => 'UserOnlineControl',
+        'RequestType' => 'LoginAsync',
+        'UE-IP' => 'ENC12bc24c4777703327f2e0aabbf6b9f9e',
+        'UE-MAC' => 'ENCCDD319C6A476FA7127DF1FB80A63CD30ADC5E47C3D',
+        'UE-Proxy' => '0',
+        'UE-Username' => $user,
+        'UE-Password' => $password
+    );
+
+    //Encode the array into JSON.
+    $jsonDataEncoded = json_encode($jsonData);
+
+    //Tell cURL that we want to send a POST request.
+    curl_setopt($ch, CURLOPT_POST, 1);
+
+    //Attach our encoded JSON string to the POST fields.
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+
+    //Set the content type to application/json
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+
+    //Execute the request
+    $result = curl_exec($ch);
+
+    /*return Response::json($results);
+    return Response()->json($Tipo);
+    */
+
   }
 
 
